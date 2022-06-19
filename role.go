4cf9d2608e00b84ce6881bbd0342ae4a76822982
Arief Darmawan <ariefda@hotmail.com>
2022-06-20 03:16:09 +0700

    fix need access

--- role.go
+++ role.go
@@ -1,8 +1,6 @@
 package kamis
 
 import (
-	"errors"
-
 	"git.kanosolution.net/kano/kaos"
 )
 
@@ -12,10 +10,11 @@ type NeedAccessOptions struct {
 	CheckFunction       func(ctx *kaos.Context, permission string, accessLevel int) error
 }
 
-func NeedAccess(permission string, needAccess NeedAccessOptions) func(*kaos.Context) (bool, error) {
+func NeedAccess(needAccess NeedAccessOptions) func(*kaos.Context) (bool, error) {
 	return func(ctx *kaos.Context) (bool, error) {
 		if needAccess.CheckFunction == nil {
-			return false, errors.New("need-access middleware is used but no implementation yet")
+			//return false, errors.New("need-access middleware is used but no implementation yet")
+			return true, nil
 		}
 
 		return true, needAccess.CheckFunction(ctx, needAccess.Permission, needAccess.RequiredAccessLevel)
